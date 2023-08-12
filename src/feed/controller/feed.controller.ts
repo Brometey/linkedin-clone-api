eed7acade7cff492727687e2284f7d48c15f15aa
Brometey <101805337+Brometey@users.noreply.github.com>
2023-08-12 13:16:50 +0600

    feed base business logic

--- src/feed/controller/feed.controller.ts
+++ src/feed/controller/feed.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { FeedService } from '../services/feed.service';
 import { FeedPost } from '../models/post.interface';
+import { Observable } from 'rxjs';
+import { DeleteResult, UpdateResult } from 'typeorm';
 
 @Controller('feed')
 export class FeedController {
@@ -9,5 +19,23 @@ export class FeedController {
   @Post()
   async create(@Body() post: FeedPost) {
     return this.feedService.createPost(post);
+  }
+
+  @Get()
+  findAll(): Observable<FeedPost[]> {
+    return this.feedService.findAllPosts();
+  }
+
+  @Put(':id')
+  update(
+    @Body() feedPost: FeedPost,
+    @Param('id') id: number,
+  ): Observable<UpdateResult> {
+    return this.feedService.updatePost(id, feedPost);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number): Observable<DeleteResult> {
+    return this.feedService.deletePost(id);
   }
 }
