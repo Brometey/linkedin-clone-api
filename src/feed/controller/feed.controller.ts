05c777c2ca01649e5d62892c4c3dfe868ce392f9
Brometey <101805337+Brometey@users.noreply.github.com>
2023-08-12 14:25:45 +0600

    pagination implemented

--- src/feed/controller/feed.controller.ts
+++ src/feed/controller/feed.controller.ts
@@ -6,6 +6,7 @@ import {
   Param,
   Post,
   Put,
+  Query,
 } from '@nestjs/common';
 import { FeedService } from '../services/feed.service';
 import { FeedPost } from '../models/post.interface';
@@ -16,14 +17,18 @@ import { DeleteResult, UpdateResult } from 'typeorm';
 export class FeedController {
   constructor(private readonly feedService: FeedService) {}
 
+  @Get()
+  findSelected(
+    @Query('take') take: number = 1,
+    @Query('skip') skip: number = 1,
+  ): Observable<FeedPost[]> {
+    take = take > 2 ? 2 : take;
+    return this.feedService.findPosts(take, skip);
+  }
+
   @Post()
   async create(@Body() post: FeedPost) {
     return this.feedService.createPost(post);
-  }
-
-  @Get()
-  findAll(): Observable<FeedPost[]> {
-    return this.feedService.findAllPosts();
   }
 
   @Put(':id')
